c7e66a0147b1f81b44ef010c6735bb8ee3f274e9
kuroibara10 <154982367+kuroibara10@users.noreply.github.com>
2024-01-15 20:35:03 +0100

    Add files via upload

---
+++ alesrt.js
@@ -0,0 +1,7 @@
+const alee = document.getElementsByClassName('alee')[0];
+const btnopen = document.getElementById('btnopen');
+
+
+btnopen.addEventListener('click', () => {
+    alee.style.display = 'block'
+})
